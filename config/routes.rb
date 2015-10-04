60caa7e267a3bbcc7bc2c2afb829a5e8a4c5ca2b
Mona2 <oh_nary@hotmail.com>
2015-10-04 13:46:19 -0700

    revote routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rottenpotatoes::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
-  root 'movies#index'
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
