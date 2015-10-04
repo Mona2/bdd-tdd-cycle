35ddc5786384ec5fae59c57b2e32f5bd3b398a2d
Mona2 <oh_nary@hotmail.com>
2015-10-04 14:22:46 -0700

    get the applicationi running

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,10 @@ Rottenpotatoes::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
-
+  get 'movies/:id/similar' => 'movies#similar', :as => :similar_movie
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :movies
+  resources :movies 
 
   # Sample resource route with options:
   #   resources :products do
